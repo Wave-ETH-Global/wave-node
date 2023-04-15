ac682796a8539f06e3be85ff12058f69b1b52bab
ChronosX88 <chronosx88@gmail.com>
2023-04-15 12:57:36 +0900

    Temporarily fix CORS

--- go.mod
+++ go.mod
@@ -5,8 +5,15 @@ go 1.20
 require github.com/labstack/echo/v4 v4.10.2
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	golang.org/x/time v0.3.0 // indirect
+)
+
+require (
 	github.com/google/logger v1.1.1 // indirect
 	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/lib/pq v1.10.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- router/router.go
+++ router/router.go
@@ -6,11 +6,18 @@ import (
 	"github.com/Wave-ETH-Global/wave-node/config"
 	"github.com/Wave-ETH-Global/wave-node/controllers"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func NewRouter(cfg *config.Config, pc *controllers.ProfileController) (*echo.Echo, error) {
 	router := echo.New()
 	router.HideBanner = true
+
+	router.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderContentType, echo.HeaderAuthorization},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+	}))
 
 	router.GET("/", func(c echo.Context) error {
 		c.String(http.StatusOK, "Hello, Wave! 🌊")
