f6271d4f9a43e1dfbf90ea8be704f5b44f0a9f5f
ChronosX88 <chronosx88@gmail.com>
2023-04-15 16:49:56 +0900

    Define basic data structure of profile and connections

--- database/migrations/001_init_schema.sql
+++ database/migrations/001_init_schema.sql
@@ -1,4 +1,23 @@
 -- +goose Up
 
+create table profile (
+   uuid text primary key,
+   eth_address text unique,
+   metadata json,
+   public_tags text[],
+   tokens json
+);
+
+create table connection (
+    id bigserial,
+    vertex_a text references profile(uuid),
+    vertex_b text references profile(uuid),
+    tags_a text[],
+    tags_b text[]
+);
+
 
 -- +goose Down
+
+drop table connection;
+drop table profile;
--- models/profile.go
+++ models/profile.go
@@ -1,8 +1,20 @@
 package models
 
 type Profile struct {
-	ETHAddress []string
-	Username   string
-	Metadata   map[string]string
-	Tags       []string
+	UUID       string                   `db:"uuid"`
+	ETHAddress string                   `db:"eth_address"`
+	Username   string                   `db:"username"`
+	Metadata   map[string]interface{}   `db:"metadata"`
+	PublicTags []string                 `db:"public_tags"`
+	Tokens     []map[string]interface{} `db:"tokens"`
+}
+
+type Connection struct {
+	ID       int64    `db:"id"`
+	VertexA  string   `db:"vertex_a"`
+	VertexB  string   `db:"vertex_a"`
+	TagsA    []string `db:"tags_a"`
+	TagsB    []string `db:"tags_b"`
+	ProfileA *Profile `db:"-"`
+	ProfileB *Profile `db:"-"`
 }
